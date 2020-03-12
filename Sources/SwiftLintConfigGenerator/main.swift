2bec658676fc76e32192197f91a28697cd2ce69c
Tyler Milner <tylermilner@gmail.com>
2020-03-11 21:51:28 -0500

    Playing around with argument parsing provided by SwiftToolsSupport package. Will eventually transition this to swift-argument-parser (https://github.com/apple/swift-argument-parser).

--- Sources/SwiftLintConfigGenerator/main.swift
+++ Sources/SwiftLintConfigGenerator/main.swift
@@ -1,4 +1,35 @@
 import SwiftLintFramework
+import TSCBasic
+import TSCUtility
+
+// TODO: Use swift-argument-parser for parsing command line arguments: https://github.com/apple/swift-argument-parser
+
+// TODO: Fill in usage/overview details
+let parser = ArgumentParser(usage: "options", overview: "Generates a .swiftlint.yml file")
+
+let disabledRulesArgument: OptionArgument<[String]> = parser.add(option: "--disabled-rules", kind: [String].self, usage: "Specify the rules to disable (e.g. `SwiftLintConfigGenerator --disabled-rules legacy_multiple force_try`)")
+
+// TODO: What other arguments to parse?
+//          - Path to save the resulting .swiftlint.yml file (otherwise just output to current directory)
+//          - Opt-in rules
+//          - Excluded directories
+//          - Custom rules?
+//          - Rule configurations (e.g. `line_length: 240`)
+
+let arguments = Array(CommandLine.arguments.dropFirst())
+
+do {
+    let parseResult = try parser.parse(arguments)
+    
+    // TODO: Disabled rules should be treated as an optional argument
+    if let disabledRules: [String] = parseResult.get(disabledRulesArgument) {
+        print("Disabled rules: \(disabledRules)")
+    } else {
+        parser.printUsage(on: stdoutStream)
+    }
+} catch {
+    parser.printUsage(on: stdoutStream)
+}
 
 let allSwiftLintRules = masterRuleList.list.keys
 
